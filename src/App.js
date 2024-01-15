b330a09e8763f883a980f535364272cae1f9fd1d
rioanggoro <rioanggoro37@gmail.com>
2024-01-15 18:30:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from "react-particles";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks";
+import { Typewriter } from "react-simple-typewriter";
+import { useState } from "react";
 
 function App() {
+  const [newYearMessage, setNewYearMessage] = useState(["Menunggu Tahun Baru"]);
+
+  const particleInitialization = async (engine) => {
+    await loadFireworksPreset(engine);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particles
+        init={particleInitialization}
+        options={{ preset: "fireworks" }}
+      />
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <span className="text-white text-4xl font-bold z-50">
+          <Typewriter words={newYearMessage} />
+        </span>
+      </div>
+    </>
   );
 }
 
